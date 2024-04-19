a2d7757c8170b95ff5ff0e13128997504b9e8bd2
omegatrix <23728542+omegatrix@users.noreply.github.com>
2024-04-19 19:41:10 +0100

    Variables

---
+++ variables/VariablesExample.java
@@ -0,0 +1,20 @@
+package variables;
+
+/**
+ * VariablesExample
+ */
+public class VariablesExample {
+    public static void main(String[] args) {
+        byte by = 127;
+        short sh = 512;
+        int in = 1024;
+        long lng = 34534;
+
+        float fl = 5.4f;
+        double dbl = 3.56;
+
+        char c = 'c';
+
+        boolean bool = true;
+    }
+}
